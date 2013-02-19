674374d954cd1f2e29709aa294e15131b384181f
Steve Kaliski <sjkaliski@gmail.com>
2013-02-19 00:48:42 -0500

    yolo

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(require('less-middleware')({ src: __dirname + '/public' }));
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+var routes = require('./routes');
+app.get('/', routes.index);
+
+server.listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.on('sendMessage', function (data, callback) {
+    callback(data);
+  });
+});
